ce00a29529fc485405fcc8f2d281c8880addbed0
JamieMaple <jamiemaple007@gmail.com>
2017-09-28 09:36:29 +0800

    react-router

---
+++ JS/react-router/router-1/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const App = () => (
+  <div>
+    <h1>App</h1>
+  </div>
+)
+
+render(
+  <App />,
+  document.getElementById('root')
+)
