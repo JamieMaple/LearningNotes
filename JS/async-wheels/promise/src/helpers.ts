2a04615dd60d0d57236a12b0162742ed87cb0aa7
JamieMaple <jamiemaple007@gmail.com>
2018-03-02 18:53:12 +0800

    complete promise first version

--- JS/async-wheels/promise/src/helpers.ts
+++ JS/async-wheels/promise/src/helpers.ts
@@ -1,97 +1,68 @@
-/*
- * helper functions
- */
-namespace promise {
-  export function isCallable(argument: any) {
-    return typeof argument === 'function'
-  }
-  
-  export function isObject(o: any): boolean {
-    const type = typeof o
-    return o !== null && type === 'object' || type === 'function'
-  }
-
-  export function isConstructor(C: any): boolean {
-    return typeof C === 'function'
-  }
-  
-  export function sameValue(x: any, y: any): boolean {
-    const typeX = typeof x
-  
-    if (typeX !== typeof y) {
-      return false
-    }
-  
-    if (typeX === 'number') {
-      // NaN, -/+ 0
-      return Object.is(x, y)
-    }
-  
-    return sameValueNonNumber(x, y)
-  }
-  
-  export function sameValueNonNumber(x: any, y: any): boolean {
-    const typeX = typeof x
-    if (typeX === 'number') {
-      throw new TypeError('type of arguments[1] is number')
-    } else if (typeX !== typeof y) {
-      throw new TypeError('two arguments types not equal')
-    }
-  
-    if (typeX == undefined) {
-      return true
-    }
-  
-    // string, boolean, symbol, object
-    return x === y
-  }
-  
-  export function isPropertyKey(argument: string) {
-    const type = typeof argument
-    return type === 'string' || type === 'symbol'
-  }
-  
-  export function getObjectProp(O: Object, P: string): any {
-    if (!isObject(O)) {
-      throw new TypeError()
-    }
-  
-    if (!isPropertyKey(P)) {
-      throw new TypeError()
-    }
-    return O[P]
-  }
-
-  export function enqueueJob(queueName: string, job: Function, arguments: List) {
-    if (typeof queueName !== 'string') {
-      throw TypeError('queueName')
-    }
-    setenqueueJob(job, 0, arguments)
-  }
-
-  export function isCompletionRecord(O: any): boolean {
-    if (O.type) {
-      return false
-    }
-    switch (O.type) {
-      case 'normal':
-      case 'return':
-      case 'throw':
-      case 'break':
-      case 'continue':
-        return true
-      default:
-        return false
-    }
-  }
-
-  export function normalCompletion(argument: any): completionRecord {
-    return {
-      type: 'normal',
-      value: argument,
-      target: null,
-    }
-  }
+export function isCallable(argument: any) {
+  return typeof argument === 'function'
 }
 
-const setenqueueJob = setTimeout
+export function isObject(o: any): boolean {
+  const type = typeof o
+  return o !== null && type === 'object' || type === 'function'
+}
+
+export function isConstructor(C: any): boolean {
+  return typeof C === 'function'
+}
+
+export function sameValue(x: any, y: any): boolean {
+  const typeX = typeof x
+
+  if (typeX !== typeof y) {
+    return false
+  }
+
+  if (typeX === 'number') {
+    // NaN, -/+ 0
+    return Object.is(x, y)
+  }
+
+  return sameValueNonNumber(x, y)
+}
+
+export function sameValueNonNumber(x: any, y: any): boolean {
+  const typeX = typeof x
+  if (typeX === 'number') {
+    throw new TypeError('type of arguments[1] is number')
+  } else if (typeX !== typeof y) {
+    throw new TypeError('two arguments types not equal')
+  }
+
+  if (typeX == undefined) {
+    return true
+  }
+
+  // string, boolean, symbol, object
+  return x === y
+}
+
+export function isPropertyKey(argument: string) {
+  const type = typeof argument
+  return type === 'string' || type === 'symbol'
+}
+
+export function getObjectProp(O: Object, P: string): any {
+  if (!isObject(O)) {
+    throw new TypeError()
+  }
+
+  if (!isPropertyKey(P)) {
+    throw new TypeError()
+  }
+  return O[P]
+}
+
+
+export function enqueueJob(queueName: string, job: Function, ...argument) {
+  if (typeof queueName !== 'string') {
+    throw TypeError('queueName')
+  }
+  let setenqueueJob
+  setTimeout(job, 0, ...argument)
+}
--- JS/async-wheels/promise/src/promise.ts
+++ JS/async-wheels/promise/src/promise.ts
@@ -1,249 +1,293 @@
-// <reference path="type.ts" />
-namespace promise {
-  export class TPromise implements IPromise {
-    public state: state
-    public value: any
-    fulfillReactions: Array<PromiseReaction>
-    rejctReactions: Array<PromiseReaction>
-    promiseIsHandlerd: boolean
-    constructor(executor: Function) {
-      if (!new.target) {
-        throw new TypeError()
-      }
-      if (!isCallable(executor)) {
-        throw new TypeError()
-      }
-      let promise = this
-      promise.state = state.PENDING
-      promise.fulfillReactions = []
-      promise.rejctReactions = []
-      promise.promiseIsHandlerd = false
-      let resolvingFunctions = createResolvingFunctions(promise)
+import {
+  IPromise,
+  STATE,
+  PromiseCability,
+  PromiseReaction,
+  completionRecord,
+} from './type'
+import {
+  isCallable,
+  isConstructor,
+  isObject,
+  isPropertyKey,
+  getObjectProp,
+  enqueueJob,
+  sameValue,
+} from './helpers'
 
-      try {
-        executor.call(undefined, resolvingFunctions.resolve, resolvingFunctions.reject)
-      } catch (reason) {
-        resolvingFunctions.reject.call(undefined, reason)
-      }
+export default class TPromise implements IPromise {
+  public state: string = STATE.PENDING
+  public value: any = undefined
+  fulfillReactions: Array<PromiseReaction> = []
+  rejctReactions: Array<PromiseReaction> = []
+  promiseIsHandlerd: boolean = false
+  constructor(executor: Function) {
+    if (!new.target) {
+      throw new TypeError()
     }
-    public then(onFulfilled: Function, onRejected: Function): IPromise {
-      let promise = this
-      if (!isPromise(promise)) {
-        throw new TypeError()
-      }
-      let speciesConstructor: Function = TPromise
-      let resultCapability = newPromiseCapability(speciesConstructor)
-      return performPromiseThen(promise, onFulfilled, onRejected, resultCapability)
+    if (!isCallable(executor)) {
+      throw new TypeError()
     }
-    public catch(onRejected: Function): IPromise {
-      return this.then(undefined, onRejected)
-    }
-  }
-  /*
-   * some Promise Abstract Operations
-   */
+    let promise = this
+    let resolvingFunctions = createResolvingFunctions(promise)
 
-  /*
-   * CreateResolvingFunctions
-   * param {promise} promise
-   * return {TRecord}
-   */
-  function createResolvingFunctions(promise: TPromise): {resolve: Function, reject: Function} {
-    let alreadyResolved: {value: boolean} = {value: false}
-
-    /* Build-in
-     * Promise Reject Functions
-     */
-    let reject: any = function(reason: any) {
-      if (!isObject(reject.promise)) {
-        throw new TypeError()
-      }
-
-      reject.promise = promise
-      reject.alreadyResolved = alreadyResolved
-
-      if (alreadyResolved.value) {
-        return undefined
-      }
-
-      alreadyResolved.value = true
-
-      return rejectPromise(promise, reason)
-    }
-    reject.promise = promise
-    reject.alreadyResolved = alreadyResolved
-
-    /* Build-in
-     * Promise Resolve Functions
-     */
-    let resolve: any = function(resolution: any) {
-      if (!isObject(reject.promise)) {
-        throw new TypeError()
-      }
-
-      resolve.promise = promise
-      resolve.alreadyResolved = alreadyResolved
-
-      if (alreadyResolved.value) {
-        return
-      }
-      
-      alreadyResolved.value = true
-
-      if (sameValue(resolution, promise)) {
-        let selfResolutionError = new TypeError('self resolution error')
-        return rejectPromise(promise, selfResolutionError)
-      }
-
-      if (!isObject(resolution)) {
-        return fulfillPromise(promise, resolution)
-      }
-
-      let then = getObjectProp(resolution, 'then')
-
-      if (isCompletionRecord(then)) {
-        return rejectPromise(promise, then.value)
-      }
-
-      let thenAction = then
-      if (isCallable(thenAction)) {
-        return fulfillPromise(promise, resolution)
-      }
-
-      enqueueJob("PromiseJobs", promiseResolveThanableJob, [promise, resolution, thenAction])
-    }
-    resolve.promise = promise
-    resolve.alreadyResolved = alreadyResolved
-    // no why
-    resolve.length = 1
-
-    return {
-      resolve,
-      reject,
-    }
-  }
-
-  /*
-   * NewPromiseCapability
-   */
-  function newPromiseCapability(C: any): PromiseCability {
-    if (!isConstructor(C)) {
-      throw TypeError()
-    }
-
-    let promiseCapability: PromiseCability = {
-      promise: undefined,
-      resolve: undefined,
-      reject: undefined,
-    }
-
-    let executor: any = function() {
-      if (executor.capability) {
-        // TODO
-      }
-      let promiseCapability = executor.capability
-      if (!promiseCapability.resolve || !promiseCapability.reject) {
-        throw new TypeError()
-      }
-    }
-    promiseCapability = executor.capability
-    let promise = new C(executor)
-    promiseCapability.promise = promise
-
-    return promiseCapability
-  }
-  /*
-   * GetCapabilitiesExecutor Functions
-   */
-
-  function rejectPromise(promise: TPromise, reason: any) {
-    if (promise.state === state.PENDING) {
-      let reactions = promise.rejctReactions
-      promise.value = reason
-      promise.fulfillReactions = promise.rejctReactions = undefined
-      promise.state = state.REJECTED
-      return triggerPromiseReactions(reactions, reason)
-    }
-  }
-
-  function fulfillPromise(promise: TPromise, value: any) {
-    if (promise.state === state.PENDING) {
-      let reactions: Array<PromiseReaction> = promise.fulfillReactions
-      promise.value = value
-      promise.fulfillReactions = promise.rejctReactions = undefined
-      promise.state = state.FULFILLED
-      return triggerPromiseReactions(reactions, value)
-    }
-  }
-
-  function triggerPromiseReactions(reactions: Array<PromiseReaction>, ...argument: List) {
-    reactions.forEach(reaction => enqueueJob('promiseJobs', promiseReactionJob, [reaction, argument]))    
-  }
-
-  function isPromise(x: any): boolean {
-    if (!isObject(x)) {
-      return false
-    }
-
-    if (x.state) {
-      return true
-    } else {
-      return false
-    }
-  }
-
-  // promise job
-  function promiseReactionJob(reaction: PromiseReaction, ...argument: List) {
-    let promiseCapability = reaction.capability
-    let type = reaction.type
-    let handler = reaction.handler
-    let handlerResult: completionRecord
-    let status
-
-    if (handler == undefined) {
-      if (type === state.FULFILLED) {
-        handlerResult = normalCompletion(argument)
-      } else if (type === state.REJECTED) {
-        handlerResult = {
-          type: 'throw',
-          value: argument,
-          target: null,
-        }
-      }
-    } else {
-      reaction.handler.call(undefined, ...argument)
-    }
     try {
-      promiseCapability.resolve.call(undefined, handlerResult.value)
+      executor.call(undefined, resolvingFunctions.resolve, resolvingFunctions.reject)
     } catch (reason) {
-      promiseCapability.reject.call(undefined, handlerResult.value)
+      resolvingFunctions.reject.call(undefined, reason)
     }
   }
-
-  function promiseResolveThanableJob(promiseToResolve: TPromise, thenable: any, then: Function) {
-    let resolvingFunctions = createResolvingFunctions(promiseToResolve)
-    try {
-      then.call(thenable, [resolvingFunctions.resolve, resolvingFunctions.reject])
-    } catch (reason) {
-      resolvingFunctions.reject.call(undefined, [reason])
+  public then(onFulfilled: Function, onRejected?: Function): IPromise {
+    let promise = this
+    if (!isPromise(promise)) {
+      throw new TypeError()
     }
+    let speciesConstructor: Function = TPromise
+    let resultCapability = newPromiseCapability(speciesConstructor)
+    return performPromiseThen(promise, onFulfilled, onRejected, resultCapability)
   }
-  // promise
-  function performPromiseThen(promise: TPromise, onFulfilled: Function, onRejected: Function, resultCapability): TPromise {
+  public catch(onRejected: Function): IPromise {
+    return this.then(undefined, onRejected)
+  }
+  public finally(): IPromise {
     // TODO
     return new TPromise(() => {})
   }
+
+  // TODO static 
+  public static all() {}
+
+  public static race() {}
+
+  public static resolve() {}
+
+  public static reject() {}
 }
 /*
-  * IsPromise
-  */
-/*
-  * GetCapabilitiesExecutor Functions
-  */
-/*
-  * promiseReactionJob
-  */
-/* 
- * Promise constructor
+ * some Promise Abstract Operations
  */
 
+/*
+ * CreateResolvingFunctions
+ * param {promise} promise
+ * return {TRecord}
+ */
+function createResolvingFunctions(promise: TPromise): {resolve: Function, reject: Function} {
+  let alreadyResolved: {value: boolean} = {value: false}
+
+  /* Build-in
+   * Promise Reject Functions
+   */
+  let reject: any = function(reason: any) {
+    if (!isObject(reject.promise)) {
+      throw new TypeError()
+    }
+
+    reject.promise = promise
+    reject.alreadyResolved = alreadyResolved
+
+    if (alreadyResolved.value) {
+      return undefined
+    }
+
+    alreadyResolved.value = true
+
+    return rejectPromise(promise, reason)
+  }
+  reject.promise = promise
+  reject.alreadyResolved = alreadyResolved
+
+  /* Build-in
+   * Promise Resolve Functions
+   */
+  let resolve: any = function(resolution: any) {
+    if (!isObject(reject.promise)) {
+      throw new TypeError()
+    }
+
+    resolve.promise = promise
+    resolve.alreadyResolved = alreadyResolved
+
+    if (alreadyResolved.value) {
+      return
+    }
+    
+    alreadyResolved.value = true
+
+    if (sameValue(resolution, promise)) {
+      let selfResolutionError = new TypeError('self resolution error')
+      return rejectPromise(promise, selfResolutionError)
+    }
+
+    if (!isObject(resolution)) {
+      return fulfillPromise(promise, resolution)
+    }
+
+    let then = getObjectProp(resolution, 'then')
+
+    let thenAction = then
+    if (!isCallable(thenAction)) {
+      return fulfillPromise(promise, resolution)
+    }
+
+    enqueueJob("PromiseJobs", promiseResolveThanableJob, promise, resolution, thenAction)
+  }
+  resolve.promise = promise
+  resolve.alreadyResolved = alreadyResolved
+
+  return {
+    resolve,
+    reject,
+  }
+}
+
+/*
+ * NewPromiseCapability
+ */
+function newPromiseCapability(C: any): PromiseCability {
+  if (!isConstructor(C)) {
+    throw TypeError()
+  }
+
+  let promiseCapability: PromiseCability = {
+    promise: undefined,
+    resolve: undefined,
+    reject: undefined,
+  }
+
+  let executor: any = function(resolve, reject) {
+    let promiseCapability = executor.capability
+
+    if (promiseCapability.resolve !== undefined || promiseCapability.reject !== undefined) {
+      throw new TypeError()
+    }
+
+    promiseCapability.resolve = resolve
+    promiseCapability.reject = reject
+  }
+  executor.capability = promiseCapability
+
+  let promise = new C(executor)
+
+  if (!isCallable(promiseCapability.resolve) || !isCallable(promiseCapability.reject)) {
+    throw new TypeError()
+  }
+  promiseCapability.promise = promise
+
+  return promiseCapability
+}
+
+/*
+ * reject functions
+ */
+function rejectPromise(promise: TPromise, reason: any) {
+  if (promise.state === STATE.PENDING) {
+    let reactions = promise.rejctReactions
+    promise.value = reason
+    promise.fulfillReactions = promise.rejctReactions = undefined
+    promise.state = STATE.REJECTED
+    return triggerPromiseReactions(reactions, reason)
+  }
+}
+
+/*
+ * resovle functions
+ */
+function fulfillPromise(promise: TPromise, value: any) {
+  if (promise.state === STATE.PENDING) {
+    let reactions: Array<PromiseReaction> = promise.fulfillReactions
+    promise.value = value
+    promise.fulfillReactions = promise.rejctReactions = undefined
+    promise.state = STATE.FULFILLED
+    return triggerPromiseReactions(reactions, value)
+  }
+}
+
+function triggerPromiseReactions(reactions: Array<PromiseReaction>, ...argument) {
+  reactions.forEach(reaction => enqueueJob('promiseJobs', promiseReactionJob, reaction, ...argument))    
+}
+
+function isPromise(x: any): boolean {
+  if (!isObject(x)) {
+    return false
+  }
+
+  if (x.state === STATE.PENDING || x.state === STATE.FULFILLED || x.state === STATE.REJECTED) {
+    return true
+  } else {
+    return false
+  }
+}
+
+// promise job
+function promiseReactionJob(reaction: PromiseReaction, argument) {
+  let promiseCapability = reaction.capability
+  let handler = reaction.handler
+  let handlerResult: completionRecord = {
+    type: 'normal',
+    value: undefined,
+    target: null
+  }
+
+  try {
+    if (<any>handler === 'Identity') {
+      handlerResult = {
+        type: 'normal',
+        value: argument,
+        target: null,
+      }
+    } else if (<any>handler === 'Thrower') {
+      handlerResult = {
+        type: 'throw',
+        value: argument,
+        target: null,
+      }
+      throw handlerResult
+    } else {
+      handlerResult.value = handler.call(undefined, argument)
+    }
+  } catch (reason) {
+    return promiseCapability.reject.call(undefined, reason)
+  }
+
+  return promiseCapability.resolve.call(undefined, handlerResult.value)
+}
+
+function promiseResolveThanableJob(promiseToResolve: TPromise, thenable: any, then: Function) {
+  let resolvingFunctions = createResolvingFunctions(promiseToResolve)
+  try {
+    then.call(thenable, resolvingFunctions.resolve, resolvingFunctions.reject)
+  } catch (reason) {
+    resolvingFunctions.reject.call(undefined, reason)
+  }
+}
+// promise
+function performPromiseThen(promise: TPromise, onFulfilled: Function, onRejected: Function, resultCapability: PromiseCability): IPromise {
+  // params not function
+  if (!isCallable(onFulfilled)) {
+    (<any>onFulfilled) = 'Identity'
+  }
+  if (!isCallable(onRejected)) {
+    (<any>onRejected) = 'Thrower'
+  }
+
+  let fulfillReaction: PromiseReaction = {
+    handler: onFulfilled,
+    capability: resultCapability,
+  }
+  let rejctReaction: PromiseReaction = {
+    handler: onRejected,
+    capability: resultCapability,
+  }
+
+  if (promise.state === STATE.PENDING) {
+    promise.fulfillReactions.push(fulfillReaction)
+    promise.rejctReactions.push(rejctReaction)
+  } else if (promise.state === STATE.FULFILLED) {
+    enqueueJob('promiseJobs', promiseReactionJob, fulfillReaction, promise.value)
+  } else if (promise.state === STATE.REJECTED) {
+    enqueueJob('promiseJobs', promiseReactionJob, rejctReaction, promise.value)
+  }
+
+  return resultCapability.promise
+}  
