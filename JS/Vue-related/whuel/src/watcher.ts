51b2598440b9af09f98e09b4a1c0d862814f9a4d
Jamie Maple <jamiemaple007@gmail.com>
2019-04-01 18:53:31 +0800

    feat(vue): watcher

--- JS/Vue-related/whuel/src/watcher.ts
+++ JS/Vue-related/whuel/src/watcher.ts
@@ -1,6 +1,20 @@
 class Watcher {
-  constructor() {
-    // TODO
+  private cb: Function;
+  private vm: IData;
+  private expOrFn: Function;
+  private depIds: object = {};
+  private getter: Function;
+
+  constructor(vm: IData, expOrFn: Function, cb: Function) {
+    this.cb = cb;
+    this.vm = vm;
+    this.expOrFn = expOrFn;
+
+    if (typeof expOrFn === 'function') {
+      this.getter = expOrFn;
+    } else {
+      // TODO
+    }
   }
 
   public get(key: string) {
